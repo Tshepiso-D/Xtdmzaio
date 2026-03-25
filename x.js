6d9981ed7bbf4e5aa1b9a25817c2a93e345beed1
Tshepiso <tsepisom658@gmail.com>
2026-03-25 17:25:26 +0200

    added some functionality

---
+++ x.js
@@ -0,0 +1,234 @@
+var retweetCount1 = document.querySelector(".retweets1")
+console.log(retweetCount1.innerHTML);
+
+retweetbutton1=document.querySelector(".retweet1");
+
+retweetbutton1.addEventListener("click", function(){
+    var retweets1=Number(retweetCount1.innerHTML);
+    retweets1+=1;
+    retweetCount1.innerHTML=retweets1;
+    console.log(retweets1);
+})
+
+var likeCount1 = document.querySelector(".likes1")
+console.log(likeCount1.innerHTML);
+
+likebutton1=document.querySelector(".like1");
+
+likebutton1.addEventListener("click", function(){
+    var likes1=Number(likeCount1.innerHTML);
+    likes1+=1;
+    likeCount1.innerHTML=likes1;
+    console.log(likes1);
+})
+
+///////
+
+var retweetCount2 = document.querySelector(".retweets2")
+console.log(retweetCount2.innerHTML);
+
+retweetbutton2=document.querySelector(".retweet2");
+
+retweetbutton2.addEventListener("click", function(){
+    var retweets2=Number(retweetCount2.innerHTML);
+    retweets2+=1;
+    retweetCount2.innerHTML=retweets2;
+    console.log(retweets2);
+})
+
+var likeCount2 = document.querySelector(".likes2")
+console.log(likeCount2.innerHTML);
+
+likebutton2=document.querySelector(".like2");
+
+likebutton2.addEventListener("click", function(){
+    var likes2=Number(likeCount2.innerHTML);
+    likes2+=1;
+    likeCount2.innerHTML=likes2;
+    console.log(likes2);
+})
+
+///////
+
+var retweetCount3 = document.querySelector(".retweets3")
+console.log(retweetCount3.innerHTML);
+
+retweetbutton3=document.querySelector(".retweet3");
+
+retweetbutton3.addEventListener("click", function(){
+    var retweets3=Number(retweetCount3.innerHTML);
+    retweets3+=1;
+    retweetCount3.innerHTML=retweets3;
+    console.log(retweets3);
+})
+
+var likeCount3 = document.querySelector(".likes3")
+console.log(likeCount3.innerHTML);
+
+likebutton3=document.querySelector(".like3");
+
+likebutton3.addEventListener("click", function(){
+    var likes3=Number(likeCount3.innerHTML);
+    likes3+=1;
+    likeCount3.innerHTML=likes3;
+    console.log(likes3);
+})
+
+///////
+
+var retweetCount4 = document.querySelector(".retweets4")
+console.log(retweetCount4.innerHTML);
+
+retweetbutton4=document.querySelector(".retweet4");
+
+retweetbutton4.addEventListener("click", function(){
+    var retweets4=Number(retweetCount4.innerHTML);
+    retweets4+=1;
+    retweetCount4.innerHTML=retweets4;
+    console.log(retweets4);
+})
+
+var likeCount4 = document.querySelector(".likes4")
+console.log(likeCount4.innerHTML);
+
+likebutton4=document.querySelector(".like4");
+
+likebutton4.addEventListener("click", function(){
+    var likes4=Number(likeCount4.innerHTML);
+    likes4+=1;
+    likeCount4.innerHTML=likes4;
+    console.log(likes4);
+})
+
+///////
+
+var retweetCount5 = document.querySelector(".retweets5")
+console.log(retweetCount5.innerHTML);
+
+retweetbutton5=document.querySelector(".retweet5");
+
+retweetbutton5.addEventListener("click", function(){
+    var retweets5=Number(retweetCount5.innerHTML);
+    retweets5+=1;
+    retweetCount5.innerHTML=retweets5;
+    console.log(retweets5);
+})
+
+var likeCount5 = document.querySelector(".likes5")
+console.log(likeCount5.innerHTML);
+
+likebutton5=document.querySelector(".like5");
+
+likebutton5.addEventListener("click", function(){
+    var likes5=Number(likeCount5.innerHTML);
+    likes5+=1;
+    likeCount5.innerHTML=likes5;
+    console.log(likes5);
+})
+
+///////
+
+var retweetCount6 = document.querySelector(".retweets6")
+console.log(retweetCount6.innerHTML);
+
+retweetbutton6=document.querySelector(".retweet6");
+
+retweetbutton6.addEventListener("click", function(){
+    var retweets6=Number(retweetCount6.innerHTML);
+    retweets6+=1;
+    retweetCount6.innerHTML=retweets6;
+    console.log(retweets6);
+})
+
+var likeCount6 = document.querySelector(".likes6")
+console.log(likeCount6.innerHTML);
+
+likebutton6=document.querySelector(".like6");
+
+likebutton6.addEventListener("click", function(){
+    var likes6=Number(likeCount6.innerHTML);
+    likes6+=1;
+    likeCount6.innerHTML=likes6;
+    console.log(likes6);
+})
+
+///////
+
+var retweetCount7 = document.querySelector(".retweets7")
+console.log(retweetCount7.innerHTML);
+
+retweetbutton7=document.querySelector(".retweet7");
+
+retweetbutton7.addEventListener("click", function(){
+    var retweets7=Number(retweetCount7.innerHTML);
+    retweets7+=1;
+    retweetCount7.innerHTML=retweets7;
+    console.log(retweets7);
+})
+
+var likeCount7 = document.querySelector(".likes7")
+console.log(likeCount7.innerHTML);
+
+likebutton7=document.querySelector(".like7");
+
+likebutton7.addEventListener("click", function(){
+    var likes7=Number(likeCount7.innerHTML);
+    likes7+=1;
+    likeCount7.innerHTML=likes7;
+    console.log(likes7);
+})
+
+///////
+
+var retweetCount8 = document.querySelector(".retweets8")
+console.log(retweetCount8.innerHTML);
+
+retweetbutton8=document.querySelector(".retweet8");
+
+retweetbutton8.addEventListener("click", function(){
+    var retweets8=Number(retweetCount8.innerHTML);
+    retweets8+=1;
+    retweetCount8.innerHTML=retweets8;
+    console.log(retweets8);
+})
+
+var likeCount8 = document.querySelector(".likes8")
+console.log(likeCount8.innerHTML);
+
+likebutton8=document.querySelector(".like8");
+
+likebutton8.addEventListener("click", function(){
+    var likes8=Number(likeCount8.innerHTML);
+    likes8+=1;
+    likeCount8.innerHTML=likes8;
+    console.log(likes8);
+})
+
+///////
+
+var retweetCount9 = document.querySelector(".retweets9")
+console.log(retweetCount9.innerHTML);
+
+retweetbutton9=document.querySelector(".retweet9");
+
+retweetbutton9.addEventListener("click", function(){
+    var retweets9=Number(retweetCount9.innerHTML);
+    retweets9+=1;
+    retweetCount9.innerHTML=retweets9;
+    console.log(retweets9);
+})
+
+var likeCount9 = document.querySelector(".likes9")
+console.log(likeCount9.innerHTML);
+
+likebutton9=document.querySelector(".like9");
+
+likebutton9.addEventListener("click", function(){
+    var likes9=Number(likeCount9.innerHTML);
+    likes9+=1;
+    likeCount9.innerHTML=likes9;
+    console.log(likes9);
+})
+
+///////
+

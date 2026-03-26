cf436df1e4a904687e24b26dc1b64802471f4cd5
Tshepiso <tsepisom658@gmail.com>
2026-03-26 13:43:52 +0200

    added some functionality

--- x.js
+++ x.js
@@ -3,6 +3,9 @@ var retweetCount1 = document.querySelector(".retweets1")
 retweetbutton1=document.querySelector(".retweet1");
 
 retweetbutton1.addEventListener("click", function(){
+    var retweetsvg=document.querySelector(".retweet");
+    retweetsvg.classList.add("retweeted");
+    retweetCount1.classList.add("retweeted");
     var retweets1=Number(retweetCount1.innerHTML);
     retweets1+=1;
     retweetCount1.innerHTML=retweets1;
@@ -13,6 +16,10 @@ var likeCount1 = document.querySelector(".likes1")
 likebutton1=document.querySelector(".like1");
 
 likebutton1.addEventListener("click", function(){
+    var likesvg=document.querySelector(".like");
+    likesvg.innerHTML=`<g><path d="M20.884 13.19c-1.351 2.48-4.001 5.12-8.379 7.67l-.503.3-.504-.3c-4.379-2.55-7.029-5.19-8.382-7.67-1.36-2.5-1.41-4.86-.514-6.67.887-1.79 2.647-2.91 4.601-3.01 1.651-.09 3.368.56 4.798 2.01 1.429-1.45 3.146-2.1 4.796-2.01 1.954.1 3.714 1.22 4.601 3.01.896 1.81.846 4.17-.514 6.67z"></path></g>`;
+    likesvg.classList.add("liked");
+    likeCount1.classList.add("liked");
     var likes1=Number(likeCount1.innerHTML);
     likes1+=1;
     likeCount1.innerHTML=likes1;
